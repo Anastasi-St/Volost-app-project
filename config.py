187bf95616f897b1cdad05c6a288413561532f60
Anastasi-St <nastya.stupnik@gmail.com>
2022-06-08 15:56:14 +0700

    config

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import secrets
 
 class Config(object):
     SECRET_KEY = secrets.token_hex()
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postpass@localhost/myappdb'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postpass@localhost/myappdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    pswrd = 'f185f5e6e0fd51c0e51d1d38d42810a04e3e757b0dbe73e297c6c24798fc2f4e'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://akrcemgftgutgk:'+pswrd+'@ec2-52-44-13-158.compute-1.amazonaws.com:5432/d7l5tct4hj9k4i'
 
     USER_APP_NAME = "Volost court protocols"
     USER_ENABLE_EMAIL = True
